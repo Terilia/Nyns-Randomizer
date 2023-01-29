ea69b1bc493c1a5127698fb02388d6ce91cb061a
Alex S <Terilia@users.noreply.github.com>
2023-01-29 18:57:25 +0100

    v0.0.4

--- src/mod.ts
+++ src/mod.ts
@@ -1,36 +1,46 @@
-import { DependencyContainer } from "tsyringe";
-
+import { DependencyContainer, inject, injectable } from "tsyringe";
 import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
 import { IPostAkiLoadMod } from "@spt-aki/models/external/IPostAkiLoadMod";
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import { ItemHelper } from "@spt-aki/helpers/ItemHelper";
+import { BaseClasses } from "@spt-aki/models/enums/BaseClasses";
 
-
+@injectable()
 class Mod implements IPreAkiLoadMod, IPostAkiLoadMod, IPostDBLoadMod {
   public postDBLoad(c: DependencyContainer) {
     //START CONFIG 
     const sillymode = false;
     const textsilly = false;
+    const everythingwitheveryting = false;
     //END CONFIG
+
+    const itemhelper = c.resolve<ItemHelper>("ItemHelper");
     const db = c.resolve<DatabaseServer>("DatabaseServer").getTables().templates.items;
     const f = ["Name", "LootExperience", "ExamineExperience", "Durability", "Accuracy", "Recoil", "Loudness", "EffectiveDistance", "Ergonomics", "Velocity", "DoubleActionAccuracyPenaltyMult", "HeatFactor", "CoolFactor", "SingleFireRate", "bFirerate", "RecoilForceUp", "RecoilForceBack", "RecoilAngle", "shotgunDispersion", "ammoAccr", "Tracer", "TracerColor", "TracerDistance", "Damage", "ProjectileCount", "RicochetChance", "MinFragmentsCount", "MaxFragmentsCount", "ShowBullet", "BallisticCoeficient", "SpeedRetardation", "InitialSpeed", "TrajectoryDeviationChance", "TrajectoryDeviation", "AmmoLifeTimeSec", "Prefab", "ItemSound"];
     const r = {};
     const a = [];
+    let allids = c.resolve<DatabaseServer>("DatabaseServer").getTables().allids
+    allids = [];
+    let mmodid = c.resolve<DatabaseServer>("DatabaseServer").getTables().mmodid
+    mmodid = []
     for (const i in db) {
       const b = db[i];
       if (!r[b._parent]) r[b._parent] = [];
       r[b._parent].push(b);
-      if (b._type == "Item" && b._parent && b._props) a.push(b);
+      if (b._type == "Item" && b._parent && b._props) {
+        a.push(b);
+        allids.push(b._id);
+      }
+
     }
+
     function s(arr) {
       for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
       }
     }
-
-    //Old
-
 
     class ProgressBar {
       public total: number;
@@ -68,36 +78,45 @@ class Mod implements IPreAkiLoadMod, IPostAkiLoadMod, IPostDBLoadMod {
         console.log();
       }
     }
-
     const progressBar = new ProgressBar(100);
     const locales = Object.values(c.resolve<DatabaseServer>("DatabaseServer").getTables().locales.global) as Record<string, string>[];
     let counter = 0;
-    if(textsilly){
-      for(const test in locales){
-        if(test == 2) { // 2 = english, this takes a long time. 
-          for(const test2 in locales[test]){
+    if (textsilly) {
+      for (const test in locales) {
+        if (test == 2) { // 2 = english, this takes a long time. 
+          for (const test2 in locales[test]) {
             counter++
-            const temp =  JSON.parse(JSON.stringify(locales[test][test2]))
+            const temp = JSON.parse(JSON.stringify(locales[test][test2]))
             let reference = Object.keys(locales[test]).length * Math.random() << 0
-            locales[test][test2] = locales[test][Object.keys(locales[test])[ reference]]
-            locales[test][Object.keys(locales[test])[ reference]] = temp
-            if (Math.random() > 0.97) {console.clear() console.log(counter + " / " + Object.keys(locales[test]).length + " Changing Textfields! OH WOW!")}
+            locales[test][test2] = locales[test][Object.keys(locales[test])[reference]]
+            locales[test][Object.keys(locales[test])[reference]] = temp
+            if (Math.random() > 0.97) { console.clear() console.log(counter + " / " + Object.keys(locales[test]).length + " Changing Textfields! OH WOW!") }
           }
         }
-  
+      }
+    }
+    for (const id in db) {
+
+      if (itemhelper.isOfBaseclasses(id, [BaseClasses.MOD, BaseClasses.FUNCTIONAL_MOD, BaseClasses.STOCK, BaseClasses.FOREGRIP, BaseClasses.MASTER_MOD, BaseClasses.MOUNT, BaseClasses.MUZZLE, BaseClasses.SIGHTS, BaseClasses.LIGHT_LASER_DESIGNATOR, BaseClasses.FLASH_HIDER, BaseClasses.IRON_SIGHT, BaseClasses.COLLIMATOR, BaseClasses.COMPACT_COLLIMATOR, BaseClasses.COMPENSATOR, BaseClasses.OPTIC_SCOPE, BaseClasses.SPECIAL_SCOPE, BaseClasses.SILENCER]) && !itemhelper.isOfBaseclasses(id, [BaseClasses.MAGAZINE, BaseClasses.CYLINDER_MAGAZINE, BaseClasses.AMMO])) {
+        mmodid.push(db[id]._id)
+        db[id]._props.RaidModdable = true;
+        db[id]._props.ToolModdable = false;
       }
     }
 
-
+    if (everythingwitheveryting) {
+      for (const id in db) {
+        if (itemhelper.isOfBaseclasses(id, [BaseClasses.WEAPON, BaseClasses.MOUNT, BaseClasses.SIGHTS])) {
+          for (const slots in db[id]._props.Slots) {
+            if (db[id]._props.Slots[slots]._name != "mod_magazine") db[id]._props.Slots[slots]._props.filters[0].Filter = mmodid.concat(db[id]._props.Slots[slots]._props.filters[0].Filter);
+          }
+        }
+      }
+    }
 
     for (const id in db) {
       if (Math.random() > 0.9) progressBar.update((Object.keys(db).indexOf(id) / Object.keys(db).length) * 100)
       for (const id2 in db[id]) {
-      //   if (sillymode && db[id]._props && db[id]._props.Cartridges && db[id]._props.Cartridges.length > 0 && db[id]._props.Cartridges[0]._max_count) {
-      //     console.log()
-      //     asdgsdgsdgsdg
-      //     db[id]._props.Cartridges[0]._max_count = Math.floor(db[id]._props.Cartridges[0]._max_count * Math.random() * (Math.random()*3))
-      // }
         for (const id3 in db[id][id2]) {
           for (let i = 0; i < a.length; i++) {
             if (
@@ -107,20 +126,22 @@ class Mod implements IPreAkiLoadMod, IPostAkiLoadMod, IPostDBLoadMod {
             ) {
 
               if (id3 == "Prefab") {
-                if (db[id]._parent == a[i]._parent && a[i][id2][id3].path && a[i][id2][id3].path != "" && db[id][id2][id3].path && db[id][id2][id3].path != "") {
+                if (id3 == "Prefab" && db[id]._parent == a[i]._parent && a[i][id2][id3].path && a[i][id2][id3].path != "" && db[id][id2][id3].path && db[id][id2][id3].path != "") {
                   const cache = JSON.parse(JSON.stringify(db[id][id2][id3]))
                   db[id][id2][id3] = JSON.parse(JSON.stringify(a[i][id2][id3]))
                   a[i][id2][id3] = JSON.parse(JSON.stringify(cache))
                   s(a);
                   break;
+                } else {
+
                 }
               } else {
                 if (sillymode) {
-                 if(Number.isInteger(db[id][id2][id3])){
-                  db[id][id2][id3] = Math.floor(db[id][id2][id3] * Math.random() * (Math.random()*10))
-                 } else{
-                  if(typeof db[id][id2][id3] == "number") db[id][id2][id3] = 2 * Math.random() * db[id][id2][id3];
-                 }
+                  if (Number.isInteger(db[id][id2][id3])) {
+                    db[id][id2][id3] = Math.floor(db[id][id2][id3] * Math.random() * (Math.random() * 10))
+                  } else {
+                    if (typeof db[id][id2][id3] == "number") db[id][id2][id3] = 2 * Math.random() * db[id][id2][id3];
+                  }
                 }
                 const cache = JSON.parse(JSON.stringify(db[id][id2][id3]))
                 db[id][id2][id3] = JSON.parse(JSON.stringify(a[i][id2][id3]))
